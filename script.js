1713542057489116ef24ef0b6df318321d1625c8
Mukangafu <dannymuturi99@gmail.com>
2025-09-18 18:12:34 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-document.getElementById('contactForm')?.addEventListener('submit', function(e) {
-    e.preventDefault();
-    alert('Thank you for contacting RENT-A-HOME!');
-});
-
 function scrollToSection(id) {
     document.getElementById(id).scrollIntoView({ behavior: 'smooth' });
 }
+
+document.getElementById('contactForm').addEventListener('submit', function(e) {
+    e.preventDefault();
+    alert('Thank you for contacting RENT-A-HOME!');
+});
